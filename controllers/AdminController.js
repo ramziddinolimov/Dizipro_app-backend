4ae9ca2953b69bfba9a8b232ab290895d4842062
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-11-24 00:46:59 +0500

    Admincontroller const admin data, ban

--- controllers/AdminController.js
+++ controllers/AdminController.js
@@ -1,1 +1,38 @@
-const AdminValidations = require()
+const AdminValidations = require("../validations/AdminValidations");
+
+module.exports = class AdminController {
+    static async CreateBanPostController(req, res, next) {
+        try {
+            const data = await AdminValidations.CreateBanValidation(
+                req.body,
+                res.error
+            );
+
+            const user = await req.db.users.findOne({
+                where: {
+                    user_id: data.user_id,
+                },
+            });
+
+            if (!user) throw new res.error(404, "User not found");
+
+            if (user.role == "admin")
+                throw new res.error(404, "You can't ban admins");
+
+                const ban = await req.db.user_bans.create({
+                    user_id: data.user_id,
+                    ban_reason: data.ban_expire_date,
+                });
+
+                res.status(201).json({
+                    ok: true,
+                    message: "Ban created",
+                    data: {
+                        ban,
+                    },
+                });
+        } catch (error) {
+            next(error);
+        }
+    }
+}
