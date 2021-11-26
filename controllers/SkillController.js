e1d712a657d8c554f8633441b1530bdb668eb378
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-11-27 00:02:52 +0500

    skill created continued

--- controllers/SkillController.js
+++ controllers/SkillController.js
@@ -3,7 +3,21 @@ const SkillValidations = require('../validations/SkillValidation')
 module.exports = class SkillsController {
     static async AddSkillPostController(req, res, next){
         try {
-            
+            const  { skill_name } = await SkillValidations.SkillCreateValidation(
+                req.body,
+                res.error
+            );
+
+            const skill = await req.db.skills.create({
+                skill_name,
+
+            });
+
+            res.status(201).json({
+                ok: true,
+                message: 'Skill created',
+                data: { skill, },
+            });
         } catch (error) {
             next(error);
         }
