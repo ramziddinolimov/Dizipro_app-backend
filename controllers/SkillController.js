1b20c039320ee8766f8cd8e03e0fb099a908bec5
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-11-26 23:59:07 +0500

    SkillConntroller js started

---
+++ controllers/SkillController.js
@@ -0,0 +1,11 @@
+const SkillValidations = require('../validations/SkillValidation')
+
+module.exports = class SkillsController {
+    static async AddSkillPostController(req, res, next){
+        try {
+            
+        } catch (error) {
+            next(error);
+        }
+    }
+}
