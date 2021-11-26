3aa01a618e8191bc761c6e0ceb95ad20364ec42d
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-11-26 20:23:39 +0500

    project controllers skills and softwares

--- controllers/ProjectController.js
+++ controllers/ProjectController.js
@@ -5,7 +5,17 @@ module.exports = class ProjectController {
     static async CreateProjectPostController(req, res, next) {
         const t = await req.db.sequelize.transaction();
         try {
-            
+            if (!(req.body?.project_skills && req.body?.project_softwares)){
+                throw new res.error(400, "Bad Request");
+            }
+            const data = await ProjectValidations.ProjectCreateValidation(
+                {
+                    ...req.body,
+                    project_skills: JSON.parse(req.body.project_skills),
+                    project_softwares: JSON.parse(req.body.project_softwares),
+                },
+                res.error
+            )
         } catch (error) {
             next(error);
         }
