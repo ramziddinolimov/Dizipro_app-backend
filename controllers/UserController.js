b91306e65b6254e34086580bdd238591c1d52d65
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-11-28 23:21:18 +0500

    UserLoginAccountPostController

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -45,4 +45,24 @@ module.exports = class UserController {
             next(error);
         }
     }
+
+    static async UserLoginAccountPostController(req, res, next){
+        try {
+            const data = await UserValidations.UserLoginAccountValidation(
+                req.body,
+                res.error
+            );
+
+            const user = await req.db.users.findOne({
+                where: {
+                    user_email: data.user_email,
+                },
+                raw: true,
+            });
+
+            if (!user) throw new res.error(404, "User not found");
+        } catch (error) {
+            next(error);
+        }
+    }
 }
