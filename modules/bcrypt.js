c4b366557e6a4d696a5e246660dcab379f198eec
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-12-03 20:41:32 +0500

    bcrypt started

---
+++ modules/bcrypt.js
@@ -0,0 +1,9 @@
+const bcrypt = require("bcrypt");
+
+module.exports.generateCrypt = function generateCrypt(password) {
+	return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+};
+
+module.exports.compareCrypt = function compareCrypt(password, crypt) {
+	return bcrypt.compareSync(password, crypt);
+};
