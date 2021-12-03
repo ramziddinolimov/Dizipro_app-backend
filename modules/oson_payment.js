2150a1383d00fbed7b42ec50296abc84707767f7
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-12-03 20:48:22 +0500

    oson-payment continued

--- modules/oson_payment.js
+++ modules/oson_payment.js
@@ -16,3 +16,24 @@ class OSON_API {
 		lifetime,
 		language
 	) {
+
+        let response = await axios({
+			url: "https://api.oson.uz/api/invoice/create",
+			headers: {
+				"Content-Type": "application/json",
+				Accept: "application/json",
+				token: this.API_KEY,
+			},
+			method: "POST",
+			data: {
+				merchant_id: this.MERCHANT_ID,
+				transaction_id,
+				amount,
+				currency,
+				user_account: user_email,
+				comment,
+				return_url,
+				lifetime,
+				language,
+			},
+		});
