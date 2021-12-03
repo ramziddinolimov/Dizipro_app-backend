7a1cd8d64e6c5c7a5813e0bbb99a60e6a0847f13
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-12-03 20:38:56 +0500

    pg started

---
+++ modules/pg/pg.js
@@ -0,0 +1,16 @@
+const { Sequelize } = require("sequelize");
+const CountryModel = require("../../models/CountryModel");
+const init = require("./init");
+const { CustomError } = require("../../helpers/CustomError");
+const UserModel = require("../../models/UserModel");
+const Relations = require("../../models/Relations");
+const UserSessionsModel = require("../../models/UserSessionsModel");
+const EmailAttempts = require("../../models/EmailAttempts");
+const BanModel = require("../../models/BanModel");
+const SkillModel = require("../../models/SkillModel");
+const SoftwareModel = require("../../models/SoftwareModel");
+const ProjectModel = require("../../models/ProjectModel");
+const ProjectsSkillModel = require("../../models/ProjectsSkillModel");
+const ProjectsSoftwares = require("../../models/ProjectsSoftwares");
+const ProjectsFilesModel = require("../../models/ProjectsFilesModel");
+const PaymentModel = require("../../models/PaymentModel");
