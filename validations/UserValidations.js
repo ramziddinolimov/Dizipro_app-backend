c17ca5ac083972252f71f1db4fd09591d8d36698
Ramziddin Olimov <ramizmafiya@gmail.com>
2021-12-09 23:05:12 +0500

    user validations finished

--- validations/UserValidations.js
+++ validations/UserValidations.js
@@ -26,3 +26,29 @@ module.exports = class UserValidations {
 				.error(new CustomError(400, "Country id is invalid")),
 		}).validateAsync(data);
 	}
+
+    static async UserLoginAccountValidation(data, CustomError) {
+		return await Joi.object({
+			user_email: Joi.string()
+				.email()
+				.required()
+				.lowercase()
+				.error(new CustomError(400, "Email is invalid")),
+
+			user_password: Joi.string()
+				.required()
+				.min(4)
+				.error(new CustomError(400, "Password is invalid")),
+		}).validateAsync(data);
+	}
+
+	static async UserForgotPasswordValidation(data, CustomError) {
+		return await Joi.object({
+			user_email: Joi.string()
+				.email()
+				.required()
+				.lowercase()
+				.error(new CustomError(400, "Email is invalid")),
+		}).validateAsync(data);
+	}
+};
